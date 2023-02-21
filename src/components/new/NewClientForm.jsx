415bec5c2f98b4361b7cad3f2cd2d83058c302c2
Silatsa Ivan <ivansilatsa@gmail.com>
2023-02-21 17:20:19 +0100

    uploading image for the profile picture

--- src/components/new/NewClientForm.jsx
+++ src/components/new/NewClientForm.jsx
@@ -1,24 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./newform.css";
-
+import profile from "../../assets/profile.png";
 function NewClientForm() {
+  // state that content user profile
+  const [ImageFile, setImageFile] = useState(null);
+
+  // a function to change the image profile of the user
+  const handleChoseImage = (image) => {
+    setImageFile(image);
+  };
+
   return (
     <div className="newFormWrap">
       <h3>Client Information</h3>
       <form action="">
         <div className="inputGrp">
           <label htmlFor="name">Full name </label>
-          <input required type="text" name="name" id="name" />
+          <input
+            required
+            placeholder="example : John Doe"
+            type="text"
+            name="name"
+            id="name"
+          />
         </div>
 
         <div className="inputGrp">
           <label htmlFor="email">Email </label>
-          <input required type="email" name="email" id="email" />
+          <input
+            placeholder="john_doe@gmail.com"
+            required
+            type="email"
+            name="email"
+            id="email"
+          />
         </div>
 
         <div className="inputGrp">
           <label htmlFor="phone">Phone number </label>
-          <input required type="text" name="phone" id="phone" />
+          <input
+            placeholder="+237 645 676 567"
+            required
+            type="text"
+            name="phone"
+            id="phone"
+          />
+        </div>
+        <div className="inputGrp">
+          <label htmlFor="profile">
+            <p>Profile Picture</p>
+            <img
+              style={{
+                cursor: "pointer",
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+              width={"80px"}
+              height={80}
+              src={ImageFile ? URL.createObjectURL(ImageFile) : profile}
+              alt="profile"
+            />
+          </label>
+          <input
+            style={{
+              display: "none",
+            }}
+            type="file"
+            name="profile"
+            id="profile"
+            onChange={(e) => handleChoseImage(e.target.files[0])}
+            accept="image/*"
+          />
         </div>
         <button>Save Client</button>
       </form>
