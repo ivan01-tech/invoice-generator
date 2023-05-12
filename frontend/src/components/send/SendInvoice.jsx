b464eabc96dc0dd475e6dbb633a80028d5f5f37e
ivan01-tech <ivansilatsa@gmail.com>
2023-05-12 14:09:26 +0200

    it's now possible to send email from the client

--- frontend/src/components/send/SendInvoice.jsx
+++ frontend/src/components/send/SendInvoice.jsx
@@ -1,24 +1,37 @@
-import React, { useEffect } from "react";
-import profile from "../../assets/profile.png";
 import "./SendInvoice.css";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import profile from "../../assets/profile.png";
+import { Link, useParams } from "react-router-dom";
 import { useAsyncFn } from "../../hooks/useAsync";
-import { sendInvoiceId } from "../../services/InvoiceCrud";
+import { getInvoiceByIdClient, sendInvoiceIdClient } from "../../services/InvoiceCrud"
 
 function SendInvoice() {
+
   const { invoice_id } = useParams();
-  const { error, loading, value, executeFn } = useAsyncFn(sendInvoiceId);
+  // to get the initial invoice values
+  const { error, loading, value, executeFn } = useAsyncFn(getInvoiceByIdClient);
+
+  // state to manage sending of an email
+  const { error: emailError, loading: emailLoad, value: emailVal, executeFn: onSendEmail } = useAsyncFn(sendInvoiceIdClient);
+
+  // the state to verify if the email was send successfully and return a feedback to the client
+  const [isEmailSuccess, setIsEmailSuccess] = useState("")
+  const [isEmailMessage, setIsEmailMessage] = useState("")
 
   const SendInvoiceHandler = async function (e) {
     e.preventDefault();
     // TODO hit the route to send the email on the server
 
-    executeFn({ invoice_id })
+    onSendEmail({ invoice_id })
       .then((res) => {
-        console.log("reeForm : ", res);
+        console.log("Form : ", res);
+        setIsEmailSuccess(true)
+        setIsEmailMessage(res.message)
       })
       .catch((err) => {
+        setIsEmailSuccess(false)
         console.log("errform : ", err);
+        setIsEmailMessage(err.message)
       });
     // const document = await console.log("doc = ", document);
   };
@@ -27,10 +40,10 @@ function SendInvoice() {
     function () {
       executeFn({ invoice_id })
         .then((res) => {
-          console.log("reeForm : ", res);
+          // console.log("reeForm : ", res);
         })
         .catch((err) => {
-          console.log("errform : ", err);
+          // console.log("errform : ", err);
         });
     },
     [invoice_id, executeFn]
@@ -39,12 +52,31 @@ function SendInvoice() {
   if (loading) return <h3>loading...</h3>;
 
   if (error) return <h3>{ error }</h3>;
+
   const totalSum = value?.items?.reduce(
     (acc, item) => acc + item.hours * item.rate,
     0
   );
+
+
+
   return (
     <div className="sendWrap">
+
+      { emailLoad && <h1>Sending email...</h1> }
+
+      {
+        emailError &&
+        <div>
+          <h1>{ isEmailMessage }</h1>
+          <Link to={ "/" }>Go back </Link> <br />
+          Or try Again
+        </div>
+      }
+      {
+        emailVal && <h1>{ emailVal?.message }</h1>
+      }
+
       <strong>Invoice</strong>
       <div className="clientInfos">
         <div>
@@ -79,13 +111,16 @@ function SendInvoice() {
           <p>Rate</p>
           <p>Hours</p>
         </div>
-        { value?.items?.map((item) => (
-          <div key={ item?._id }>
-            <strong>{ item.title }</strong>
-            <strong>{ item.hours }</strong>
-            <strong>{ item.rate }</strong>
-          </div>
-        )) }
+        {/* items */ }
+        {
+          value?.items?.map((item) => (
+            <div key={ item?._id }>
+              <strong>{ item.title }</strong>
+              <strong>{ item.hours }</strong>
+              <strong>{ item.rate }</strong>
+            </div>
+          ))
+        }
       </div>
       <div className="totalPrice">
         <h4>Total: US$ { totalSum }</h4>
--- frontend/src/services/InvoiceCrud.js
+++ frontend/src/services/InvoiceCrud.js
@@ -1,20 +1,27 @@
-import { makeRequest } from "./makeRequest"
+import { makeRequest } from "./makeRequest";
 
 /**
- * to add new invoice 
- * @param {{userId:string,items:Array}}} params 
- * @returns 
+ * to add new invoice
+ * @param {{userId:string,items:Array}}} params
+ * @returns
  */
 export const createInvoice = function (params) {
-    return makeRequest("/invoices", {
-        method: "POST",
-        data: JSON.stringify(params)
-    })
-}
+  return makeRequest("/invoices", {
+    method: "POST",
+    data: JSON.stringify(params),
+  });
+};
 
-export const sendInvoiceId = function ({ invoice_id }) {
-    return makeRequest(`/invoices/${invoice_id}`, {
-        method: "POST",
-        data: JSON.stringify({ invoice_id })
-    })
-}
+export const getInvoiceByIdClient = function ({ invoice_id }) {
+  return makeRequest(`/invoices/${invoice_id}`, {
+    method: "POST",
+    data: JSON.stringify({ invoice_id }),
+  });
+};
+
+export const sendInvoiceIdClient = function ({ invoice_id }) {
+  return makeRequest(`/invoices/${invoice_id}/send_email`, {
+    method: "POST",
+    data: JSON.stringify({ invoice_id }),
+  });
+};
